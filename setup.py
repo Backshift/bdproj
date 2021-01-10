f78b6b039e4e665dc2c87839bf40723f7a5f66a4
Backshift <35261400+Backshift@users.noreply.github.com>
2021-01-10 18:32:39 +0100

    New SQL Architecture, New Package

--- setup.py
+++ setup.py
@@ -13,33 +13,71 @@ def setup_pckges(packages):
                 __import__(package)
                 print("Successfully imported", package)
             except ModuleNotFoundError:
-                print("except")
                 import subprocess
                 import sys
                 subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
 
+def setup_database():
+    import sqlite3
+    with sqlite3.connect('BD_Project.sqlite') as conn:
+        conn.execute("PRAGMA foreign_keys = 1")
+        cur = conn.cursor()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Names(
+                nameid TEXT PRIMARY KEY,
+                name TEXT, --Name given to Baby
+                gender TEXT);
+                ''')
+        conn.commit()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Year_Data(
+                nameid TEXT,
+                counts INTEGER, --How many that Year
+                year INTEGER, --Which year this entry was in
+                UNIQUE(nameid, year), --Unique constraint to avoid duplicates
+                FOREIGN KEY(nameid) REFERENCES Names(nameid));
+                ''')
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Actors(
+                actid TEXT PRIMARY KEY, --actors imdb id
+                actname TEXT); --actors name
+            ''')
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Movies(
+                movid TEXT PRIMARY KEY, --imdB ID of that movie/series
+                title TEXT, --Title of content (e.g. "Star Trek: The Next Generation")
+                year INTEGER); --Year of Release or First episode
+            ''')
+        conn.commit()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Cast(
+                movid TEXT,
+                actid TEXT,
+                FOREIGN KEY(actid) REFERENCES Actors(actid),
+                FOREIGN KEY(movid) REFERENCES Movies(movid));
+                ''')
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS Selected(
+                movid TEXT, --imdB ID of that movie/series
+                actid TEXT,
+                actname TEXT,
+                charname TEXT,
+                FOREIGN KEY(movid) REFERENCES Movies(movid),
+                FOREIGN KEY(actid) REFERENCES Actors(actid));
+            ''')
+        conn.commit()
 
 if __name__ == "__main__":
-    needed_packages = list()
-    needed_packages.append('wheel')
-    needed_packages.append('requests')
-    needed_packages.append('urllib')
-    needed_packages.append('ssl')
-    needed_packages.append('json')
-    needed_packages.append('re')
-    needed_packages.append(['sqlite3', 'pysqlite'])
-    needed_packages.append('concurrent.futures')
-    needed_packages.append('multiprocessing')
-    needed_packages.append('threading')
-    needed_packages.append('joblib')
-    needed_packages.append('os')
-    needed_packages.append('math')
-    needed_packages.append('datetime')
-    needed_packages.append('time')
-    needed_packages.append('numpy')
-    needed_packages.append('pandas')
-    needed_packages.append('plotly')
-    needed_packages.append('dash')
-    needed_packages.append(['progressbar', 'progessbar2'])
-
-    setup_pckges(needed_packages)
+    inp = input("This Script will install needed packages by using the pip command.\nProceed? (y/n)\n").strip(",.- ")
+    while 1:
+        if inp == "y" or inp == "Y":
+            needed_packages = ['wheel', 'requests', 'urllib', 'ssl', 'json', 're', ['sqlite3', 'pysqlite'], 'concurrent.futures', 'multiprocessing', 'threading', 'joblib', 'os', 'math', 'datetime', 'time', 'numpy', 'pandas', 'plotly', 'dash', ['progressbar', 'progressbar2']]
+            setup_pckges(needed_packages)
+            break
+        elif inp == "n" or inp == "N":
+            print("The Code to execute wont work without these libraries. Exiting...")
+            exit()
+        else:
+            inp = input("Try again:\n")
+    print("Setting up database")
+    setup_database()
